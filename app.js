b5257c15c995ada2019c875a5789abd05eea92b0
WaelS87 <wfakhrji@gmail.com>
2022-05-30 16:33:27 -0300

    estructura inicial

---
+++ app.js
@@ -0,0 +1,28 @@
+//para cmbiar de jeson a javascribt:
+/*const fs = require('fs');
+const studintJs = fs.readFileSync('./studint.json','utf-8');
+const info = JSON.parse(studintJs);
+console.log(info);*/
+
+// otra manera para cambiar de jeson a jv:
+
+/*const alumno = require('./studint.json');
+console.log(alumno);*/
+
+console.log("***********************************************************");
+//paqr cambiar de js a jeson:
+let info = {
+    nombre:"Wael",
+    apellido:"fakhrji",
+    nacionalidad:"siria"};
+
+/*const fs = require('fs');*/
+//const infoJson = JSON.stringify(info,null,3);
+//const informacion =fs.writeFileSync('./alumno.json',infoJson);
+//console.log(infoJson);*/
+
+
+/*
+const object =require('./app.js');
+const infoJson = JSON.stringify(info,null,3);
+console.log(infoJson);*/
